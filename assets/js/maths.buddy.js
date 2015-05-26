9b995e43bc019868827ad16f302e92db933a0ebd
PanDoreS <xxpandoresxx@gmail.com>
2015-05-26 15:57:41 +0200

    Fix & Add methods
    - Fix table input text “loi binomiale”
    - Design “Variable aléatoire d'une série statistique”
    - Fix + / - dynamic button CSS
    - Copy values dynamic + button into the previously created div
    - Fix dynamic id name -> Statistiques
    
    TODO :
    - Add on load jQuery -> Select active menu by anchor (desynchro
    possible)

--- assets/js/maths.buddy.js
+++ assets/js/maths.buddy.js
@@ -3,94 +3,107 @@
 */
 $(document).ready(function() {
 
-
+ detect_hash_dynamic_menu();
 
 $(window).scroll(function () {
-     var anchor = document.location.hash;
-     if (anchor === '#maths') {
-      if($('#maths').visible()){
-       $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-       $("#maths-menu").addClass("active_jquery"); //Add "active" class to selected tab
-      }
-      }else{
-       if($('#top').visible()){
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#top-menu").addClass("active_jquery"); //Add "active" class to selected tab
-       }else if ($('#manuel').visible()){
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#manuel-menu").addClass("active_jquery"); //Add "active" class to selected tab
-       }else if($('#menu').visible()){
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#menu-menu").addClass("active_jquery"); //Add "active" class to selected tab
-       }else if($('#webapp').visible()){
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#webapp-menu").addClass("active_jquery"); //Add "active" class to selected tab
-       }else if($('#about').visible()){
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#about-menu").addClass("active_jquery"); //Add "active" class to selected tab
-       }else if($('#contact').visible()){
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#contact-menu").addClass("active_jquery"); //Add "active" class to selected tab
-       }
-      }
+ detect_hash_dynamic_menu();
 });
 
-    var anchor = document.location.hash;
-    if (anchor === '#maths') {
-        $("#maths_window").css({
-            "display": "block"
-        });
-        $("#main_window").css({
-            "display": "none"
-        });
-        //Fix desynchro div - to be updated
-        $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-        $("#maths-menu").addClass("active_jquery"); //Add "active" class to selected tab
-        $("html, body").animate({
-            scrollTop: $(anchor).offset().top
-        }, 400);
-    } else if (anchor === '') {
-     $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-     $("#top-menu").addClass("active_jquery"); //Add "active" class to selected tab
-    }else{
-        $("#maths_window").css({
-            "display": "none"
-        });
-        $("#main_window").css({
-            "display": "block"
-        });
-        $("html, body").animate({
-            scrollTop: $(anchor).offset().top
-        }, 400);
-    }
+function detect_hash_dynamic_menu() {
+ if ($("#maths_window").css('display') == 'block') {
+  if($('#maths').visible()){
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#maths-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#second-degre').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#second-degre-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#statistiques').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#statistiques-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#loi-binomiale').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#loi-binomiale-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#loi-normale').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#loi-normale-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#loi-poisson').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#loi-poisson-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#variable-aleatoire').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#variable-aleatoire-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }else if ($('#test-khi-2').visible()) {
+   $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+   $("#test-khi-2-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  }
+  }else{
+   if($('#top').visible()){
+    $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+    $("#top-menu").addClass("active_jquery"); //Add "active" class to selected tab
+   }else if ($('#manuel').visible()){
+    $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+    $("#manuel-menu").addClass("active_jquery"); //Add "active" class to selected tab
+   }else if($('#menu').visible()){
+    $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+    $("#menu-menu").addClass("active_jquery"); //Add "active" class to selected tab
+   }else if($('#webapp').visible()){
+    $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+    $("#webapp-menu").addClass("active_jquery"); //Add "active" class to selected tab
+   }else if($('#about').visible()){
+    $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+    $("#about-menu").addClass("active_jquery"); //Add "active" class to selected tab
+   }else if($('#contact').visible()){
+    $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+    $("#contact-menu").addClass("active_jquery"); //Add "active" class to selected tab
+   }
+  }
+}
 
-    $(window).bind('hashchange', function(e) {
-        var anchor = document.location.hash;
-        if (anchor === '#maths') {
-            $("#maths_window").css({
-                "display": "block"
-            });
-            $("#main_window").css({
-                "display": "none"
-            });
-            //Fix desynchro div - to be updated
-            $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
-            $("#maths-menu").addClass("active_jquery"); //Add "active" class to selected tab
-        } else {
-            $("#maths_window").css({
-                "display": "none"
-            });
-            $("#main_window").css({
-                "display": "block"
-            });
-        }
-        $("html, body").animate({
-            scrollTop: $(anchor).offset().top
-        }, 400);
-    });
+hash_change_div();
+
+$(window).bind('hashchange', function(e) {
+ hash_change_div();
 });
+
+function hash_change_div() {
+ var anchor = document.location.hash;
+ if (anchor === '#maths' || anchor === '#second-degre' || anchor === '#statistiques' || anchor === '#loi-binomiale' || anchor === '#loi-normale' || anchor === '#loi-poisson' || anchor === '#variable-aleatoire' || anchor === '#test-khi-2') {
+     $("#maths_window").css({
+         "display": "block"
+     });
+     $("#main_window").css({
+         "display": "none"
+     });
+     $("html, body").animate({
+         scrollTop: $(anchor).offset().top
+     }, 400);
+
+ } else if (anchor === '') {
+  $("#nav li a.active_jquery").removeClass("active_jquery"); //Remove any "active" class
+  $("#top-menu").addClass("active_jquery"); //Add "active" class to selected tab
+  $("#maths_window").css({
+      "display": "none"
+  });
+  $("#main_window").css({
+      "display": "block"
+  });
+ }else{
+     $("#maths_window").css({
+         "display": "none"
+     });
+     $("#main_window").css({
+         "display": "block"
+     });
+     $("html, body").animate({
+         scrollTop: $(anchor).offset().top
+     }, 400);
+ }
+}
+
+//Set radio button
 $("#type-saisie-loi-poisson").buttonset();
 $("#type-saisie-loi-normale").buttonset();
+//Dynamic radio button
 $('#radio-type-loi-poisson-1').click(function() {
     $("#lambda_poisson").css({
         "display": "none"
@@ -107,6 +120,7 @@ $('#radio-type-loi-poisson-2').click(function() {
         "display": "none"
     });
 });
+
 $('.button-add-statistiques').click(function() {
     var currentVal = parseInt($("#nombre_vars_statistiques").val());
     if (!isNaN(currentVal)) {
@@ -115,12 +129,16 @@ $('.button-add-statistiques').click(function() {
     }
     $('#add-div-statistiques').append('<div id="add-otf-statistiques_' +
         currentVal +
-        '"><a class="button scrolly button-remove-statistiques" onClick="$(this).Remove_Statistiques_Row(' +
+        '"><a class="button scrolly button-remove-statistiques" onClick="$(this).Remove_Row(' +
         currentVal +
-        ');" style="float: right; padding-top: 0.348em; padding-bottom: 0.388em;padding-left:0.792em;padding-right:0.792em;font-size:1.5em;">-</a><div style="padding-right: .5em;padding-bottom:2em;position:absolute;width:62%;"><input type="text" name="statistiques_numero_' +
-        currentVal + '" id="statistiques_numero_"' + currentVal +
-        ' placeholder="Nombre" style="width:100%"/></div><br/><br/></div>'
+        ', 1);" style="float: right; padding-top: 12.5px; padding-bottom: 12.5px;padding-left:29px;padding-right:29px; font-size:1.5em;">-</a><div style="padding-right: .5em;padding-bottom:2em;position:absolute;width:62%;"><input type="text" name="statistiques_numero_' +
+        currentVal + '" id="statistiques_numero_' + currentVal +
+        '" placeholder="Nombre" style="width:100%"/></div><br/><br/></div>'
     );
+    //Copy & reset values on the newly created div
+    $("#statistiques_numero_" + currentVal).val($("#statistiques_numero_1").val());
+    $("#statistiques_numero_1").val("");
+
     if (!$('#send-button-statistiques').visible()) {
         $("html, body").animate({
             scrollTop: $('#send-button-statistiques').offset().top +
@@ -128,11 +146,56 @@ $('.button-add-statistiques').click(function() {
         }, 400);
     }
 });
-$.fn.Remove_Statistiques_Row = function(id) {
-    var currentVal = parseInt($("#nombre_vars_statistiques").val());
-    if (!isNaN(currentVal)) {
-        currentVal = currentVal - 1;
-        $("#nombre_vars_statistiques").val(currentVal);
-    }
-    $(this).closest("#add-otf-statistiques_" + id).remove();
+$.fn.Remove_Row = function(id, type) {
+ if( type == 1 ){
+     var currentVal = parseInt($("#nombre_vars_statistiques").val());
+     if (!isNaN(currentVal)) {
+         currentVal = currentVal - 1;
+         $("#nombre_vars_statistiques").val(currentVal);
+     }
+     $(this).closest("#add-otf-statistiques_" + id).remove();
+ } else if ( type == 2 ){
+
+     var currentVal = parseInt($("#nombre_vars_v_aleatoire").val());
+     if (!isNaN(currentVal)) {
+         currentVal = currentVal - 1;
+         $("#nombre_vars_v_aleatoire").val(currentVal);
+     }
+     $(this).closest("#add-otf-v-aleatoire_" + id).remove();
+ }
 };
+
+$('.button-add-v-aleatoire').click(function() {
+    var currentVal = parseInt($("#nombre_vars_v_aleatoire").val());
+    if (!isNaN(currentVal)) {
+        currentVal = currentVal + 1;
+        $("#nombre_vars_v_aleatoire").val(currentVal);
+    }
+    $('#add-div-v-aleatoire').append('<div id="add-otf-v-aleatoire_' +
+    currentVal +
+    '"><a class="button scrolly button-remove-statistiques" onClick="$(this).Remove_Row(' +
+    currentVal +
+    ', 2);" style="float: right; padding-top: 12.5px; padding-bottom: 12.5px;padding-left:29px;padding-right:29px; font-size:1.5em;">-</a><div style="padding-right: .5em;padding-bottom:2em;position:absolute;width:62%;"><table><tr><td style="border-right: 10px solid transparent;-webkit-background-clip: padding;-moz-background-clip: padding;background-clip: padding-box;"><input id="v_aleatoire_x_' +
+    currentVal +
+    '" name="v_aleatoire_x_' +
+    currentVal +
+    '" placeholder="X" style="width:100%" type="text"></td><td><input id="v_aleatoire_proba_x_' +
+    currentVal +
+    '" name="v_aleatoire_proba_x_' +
+    currentVal +
+    '" placeholder="Probabilité de X" style="width:100%" type="text"></td></tr></table></div><br/><br/></div>'
+    );
+    //Copy & reset values on the newly created div
+    $("#v_aleatoire_x_" + currentVal).val($("#v_aleatoire_x_1").val());
+    $("#v_aleatoire_proba_x_" + currentVal).val($("#v_aleatoire_proba_x_1").val());
+    $("#v_aleatoire_x_1").val("");
+    $("#v_aleatoire_proba_x_1").val("");
+
+    if (!$('#send-button-v-aleatoire').visible()) {
+        $("html, body").animate({
+            scrollTop: $('#send-button-v-aleatoire').offset().top +
+                -$(window).height() + 130
+        }, 400);
+    }
+});
+});
